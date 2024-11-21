84419b6314ed3f134315aba3720da599786189a8
malcolmcdixon <56974882+malcolmcdixon@users.noreply.github.com>
2024-11-21 14:09:28 +0000

    change plan cost to int, use push_front when adding actions to plan

--- goap/plan.gd
+++ goap/plan.gd
@@ -4,7 +4,7 @@ class_name GoapPlan
 static var NO_PLAN: GoapPlan
 
 var actions: Array[GoapAction]
-var cost: float
+var cost: int
 
 
 static func _static_init() -> void:
@@ -12,14 +12,14 @@ static func _static_init() -> void:
 
 
 # Constructor to initialize the plan with actions and cost
-func _init(plan_actions: Array[GoapAction] = [], plan_cost: float = 0.0) -> void:
+func _init(plan_actions: Array[GoapAction] = [], plan_cost: int = 0) -> void:
 	self.actions = plan_actions
 	self.cost = plan_cost
 
 
 # Add action to the plan and update cost
-func add_action(action: GoapAction, action_cost: float) -> void:
-	actions.append(action)
+func add_action(action: GoapAction, action_cost: int) -> void:
+	actions.push_front(action)
 	cost += action_cost
 
 
