8d859f3f28a3d4e11e391259d47a210e2c3afc89
malcolmcdixon <56974882+malcolmcdixon@users.noreply.github.com>
2024-11-30 21:28:30 +0000

    LF to CRLF

--- goap/actions/find_cover.gd
+++ goap/actions/find_cover.gd
@@ -5,7 +5,9 @@ class_name FindCoverAction
 
 func _init() -> void:
 	preconditions.append(GoapState.new(Goap.States.IS_FRIGHTENED, true))
+
 	effects.append(GoapState.new(Goap.States.PROTECTED, true))
+
 	strategy = MoveToTargetActionStrategy.new("cover", 1.0)
 
 
