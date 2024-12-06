6b7bc686167d8c18129b5f74fe13cd6a7b310886
malcolmcdixon <56974882+malcolmcdixon@users.noreply.github.com>
2024-12-06 16:39:44 +0000

    use GoapCondition instead of Callable for conditions

--- goap/rule.gd
+++ goap/rule.gd
@@ -3,14 +3,20 @@ extends RefCounted
 
 
 var name: StringName
-var condition: Callable
+var condition: GoapCondition
 var effects: Array[GoapState]
+
 
 func _init(
 	init_name: StringName,
-	init_condition: Callable,
+	init_condition: GoapCondition,
 	init_effects: Array[GoapState]
 ) -> void:
 	self.name = init_name
 	self.condition = init_condition
 	self.effects = init_effects
+
+
+func process(input: Variant) -> void:
+	if condition and condition.evaluate(input):
+		Goap.world_state.apply_effects(effects)
